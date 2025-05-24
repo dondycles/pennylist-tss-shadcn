7015b786662110898007b4c617533da33d8e4822
dondycles <johnrod.dondoyano@gmail.com>
2025-05-24 10:28:57 +0800

    with service worker

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "@tanstack/react-start/config";
+import { VitePWA } from "vite-plugin-pwa";
 import tsConfigPaths from "vite-tsconfig-paths";
-
 export default defineConfig({
   vite: {
     plugins: [
@@ -9,6 +9,41 @@ export default defineConfig({
         projects: ["./tsconfig.json"],
       }),
       tailwindcss(),
+      VitePWA({
+        registerType: "autoUpdate",
+        injectRegister: "script",
+        devOptions: { enabled: true },
+        includeAssets: ["favicon.ico", "icon-256.png", "icon-512.png", "summary.png"],
+        manifest: {
+          name: "pennylist.",
+          short_name: "pennylist.",
+          description: "Avoid becoming penniless, start using pennylist.",
+          start_url: "/",
+          scope: "/",
+          display: "standalone",
+          background_color: "#000000",
+          theme_color: "#000000",
+          icons: [
+            {
+              src: "/favicon.ico",
+              sizes: "any",
+              type: "image/x-icon",
+            },
+            {
+              src: "/icon-512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "maskable",
+            },
+            {
+              src: "/icon-256.png",
+              sizes: "256x256",
+              type: "image/png",
+              purpose: "any",
+            },
+          ],
+        },
+      }),
     ],
   },
 
