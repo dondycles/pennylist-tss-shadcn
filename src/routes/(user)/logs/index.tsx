402f4ab587c00417eb356afca0bfd3ffda7a11e0
dondycles <johnrod.dondoyano@gmail.com>
2025-05-30 14:06:05 +0800

    FIlter search now popUps via dialog

--- src/routes/(user)/logs/index.tsx
+++ src/routes/(user)/logs/index.tsx
@@ -1,3 +1,11 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/animate-ui/radix/dialog";
 import LogCard from "@/components/LogCard";
 import PageStatusSetter from "@/components/PageStatusSetter";
 import Scrollable from "@/components/Scrollable";
@@ -10,7 +18,7 @@ import { moneyIdsQueryOptions } from "@/lib/queries/money";
 import { debounce } from "@tanstack/pacer";
 import { useSuspenseInfiniteQuery, useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute, Link } from "@tanstack/react-router";
-import { FileClock, RefreshCw, X } from "lucide-react";
+import { FileClock, Filter, RefreshCw, X } from "lucide-react";
 import React, { Suspense } from "react";
 export const Route = createFileRoute("/(user)/logs/")({
   component: RouteComponent,
@@ -38,23 +46,6 @@ export const Route = createFileRoute("/(user)/logs/")({
 });
 
 function RouteComponent() {
-  const navigate = Route.useNavigate();
-  const {
-    user,
-    queryClient,
-    search: { q, flow, type, money },
-  } = Route.useRouteContext();
-  const debouncedSearch = debounce(
-    async (searchTerm: string) => {
-      await navigate({ to: "/logs", search: { q: searchTerm, flow, money, type } });
-      await queryClient.invalidateQueries({
-        queryKey: ["logs", user?.id, flow, type, money, q],
-      });
-    },
-    {
-      wait: 500,
-    },
-  );
   return (
     <Scrollable hideTotalMoney={true}>
       <div className="text-muted-foreground flex items-center justify-between gap-4 border-b p-4">
@@ -66,19 +57,7 @@ function RouteComponent() {
           <RefreshCw className="size-4" />
         </button>
       </div>
-      <div className="px-4">
-        <Input
-          onChange={(e) => debouncedSearch(e.currentTarget.value)}
-          placeholder="Search"
-        />
-      </div>
-      <Suspense
-        fallback={
-          <p className="text-muted-foreground text-center text-sm">Getting moneys...</p>
-        }
-      >
-        <MoneyIds />
-      </Suspense>
+      <SearchInput />
       <Suspense
         fallback={
           <p className="text-muted-foreground text-center text-sm">Getting logs...</p>
@@ -98,16 +77,106 @@ function RouteComponent() {
   );
 }
 
+function SearchInput() {
+  const navigate = Route.useNavigate();
+
+  const {
+    user,
+    queryClient,
+    search: { q, flow, type, money },
+  } = Route.useRouteContext();
+  const debouncedSearch = debounce(
+    async (searchTerm: string) => {
+      await navigate({ to: "/logs", search: { q: searchTerm, flow, money, type } });
+      await queryClient.invalidateQueries({
+        queryKey: ["logs", user?.id, flow, type, money, q],
+      });
+    },
+    {
+      wait: 500,
+    },
+  );
+  return (
+    <div className="flex px-4">
+      <Input
+        id="searchLog"
+        className="-me-px rounded-e-none shadow-none focus-visible:z-10"
+        placeholder="Search for 'reason'"
+        type="text"
+        onChange={(e) => debouncedSearch(e.currentTarget.value)}
+      />
+      <Dialog>
+        <DialogTrigger className="text-muted-foreground flex items-center gap-2 rounded-s-none rounded-e-full border pr-4 pl-3 text-sm">
+          <Filter className="size-4" />
+          <span>Filter</span>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Filter Logs</DialogTitle>
+            <DialogDescription>
+              Use the filters to narrow down your log search results. You can filter by
+              type, money, and more.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="space-y-4">
+            <div className="space-y-2">
+              <p className="text-muted-foreground text-sm">Moneys:</p>
+              <Suspense
+                fallback={
+                  <p className="text-muted-foreground text-center text-sm">
+                    Getting moneys...
+                  </p>
+                }
+              >
+                <MoneyIds />
+              </Suspense>
+            </div>
+            <div className="space-y-2">
+              <p className="text-muted-foreground text-sm">Type:</p>
+              <div className="flex flex-wrap gap-2">
+                {["add", "edit", "delete", "transfer"].map((_type) => (
+                  <Badge
+                    asChild
+                    className="rounded-full text-sm"
+                    variant={"outline"}
+                    key={_type}
+                  >
+                    <Link
+                      to="/logs"
+                      search={{
+                        type:
+                          type === _type
+                            ? undefined
+                            : (_type as "add" | "edit" | "delete" | "transfer"),
+                        flow,
+                        money,
+                        q,
+                      }}
+                    >
+                      {_type}
+                      {type === _type ? <X /> : null}
+                    </Link>
+                  </Badge>
+                ))}
+              </div>
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
+
 function MoneyIds() {
   const { search, user } = Route.useRouteContext();
   const moneyIds = useSuspenseQuery(moneyIdsQueryOptions(user?.id));
   return (
-    <div className="flex flex-wrap gap-2 px-4">
+    <div className="flex flex-wrap gap-2">
       {moneyIds.data.map((money) => (
         <Link
           key={money.id}
           to="/logs"
-          search={{ money: money.id === search.money ? "" : money.id }}
+          search={{ ...search, money: money.id === search.money ? undefined : money.id }}
         >
           <Badge
             style={{
@@ -164,7 +233,7 @@ function Logs() {
               return <LogCard log={log} key={log.id} />;
             })}
             <Button
-              className="text-muted-foreground text-xs font-light"
+              className="text-muted-foreground w-full text-center text-sm font-light"
               hidden={!logs.hasNextPage}
               ref={loaderRef}
               variant={"ghost"}
@@ -189,7 +258,7 @@ function Logs() {
             return <LogCard log={log} key={log.id} />;
           })}
           <Button
-            className="text-muted-foreground text-xs font-light"
+            className="text-muted-foreground w-full text-center text-sm font-light"
             hidden={!logs.hasNextPage}
             ref={loaderRef}
             variant={"ghost"}
